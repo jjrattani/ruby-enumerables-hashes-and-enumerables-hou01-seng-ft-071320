21bdc6ff2a219de6231a45820f7e588c853f1a07
Jessica  Rattani <jjrattani@gmail.com>
2020-07-03 03:34:00 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key , value|
-    if key == "suite_a:" && value.first == "A"
+    if key == :suite_a && value.first == "A"
       puts value
     end
   end
